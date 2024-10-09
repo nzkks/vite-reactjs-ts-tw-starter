7230dc4eb22403f30a79594bc93c39bc0e90c924
nzkks <kkshanthosh@gmail.com>
2024-10-09 21:37:36 +1300

    minor

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
